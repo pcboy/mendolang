2df3ccb01e020502f408d5a0b81891d2903a31e3
pcboy <unknown>
2012-04-20 20:34:35 +0900

    Add forgotten license (WTFPL) and usage.

--- mendolang.rb
+++ mendolang.rb
@@ -1,6 +1,28 @@
 #!/usr/bin/env ruby
+#
+#          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                  Version 2, December 2004
+#
+#  Copyright (C) 2004 Sam Hocevar
+#  14 rue de Plaisance, 75014 Paris, France
+#  Everyone is permitted to copy and distribute verbatim or modified
+#  copies of this license document, and changing it is allowed as long
+#  as the name is changed.
+#  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+#
+#  David Hagege <david.hagege@gmail.com>
+#
+
 require 'yaml'
 require 'smart_colored/extend'
+
+if ARGV.empty?
+  $stderr.puts "Usage: #{$0} config/locales/*.yml"
+  exit
+end
 
 no_colors = ARGV[0] == '--no-colors'
 ARGV.shift if no_colors
